196d4499f33a8433df0a96525212cdd6bd73e01c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-07-26 23:25:33 -0600

    trying new things

--- basic/main.py
+++ basic/main.py
@@ -1,12 +1,17 @@
 from manimlib import *
 
+CONFIG = {
+    "camera_config":{"background_color": "#003399"}
+}
 class SquareToCircle(Scene):
     def construct(self):
+        # Setting background color
         circle = Circle()
         square = Square()
         square.flip(RIGHT)
         square.rotate(-3 * TAU / 8)
         circle.set_fill(RED, opacity=0.5)
+        circle.set_stroke(BLUE_E, width=10)
         self.play(FadeIn(square))
         self.play(Transform(square, circle))
         # self.play(FadeOut(square))
